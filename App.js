1286d4cdc0961806219b4bf91430c3040d17fb8f
a.saidenes@gmail.com <a.saidenes@gmail.com>
2020-12-14 09:08:36 +0300

    todoapp uygulaması yapıldı

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import Header from './compenents/header';
+import TodoItem from './compenents/todoItem'; 
+import AddTodo from './compenents/addTodo';
+
+
+export default function App  () {
+  const [todos, setTodos] = useState([
+    //{text : 'buy coffee',  key : '1'  },
+    //{text : 'create an app',  key : '2'  },
+    //{text : 'play on the switch',  key : '3'  },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+  const submitHandler = (text) => {
+    setTodos((prevTodos) =>{
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ];
+    })
+  }
+  return (
+    <TouchableWithoutFeedback onPress ={() => {
+      Keyboard.dismiss();
+    }}>
+    <View style={styles.container}>
+      <Header />
+      <View style = {styles.content}>
+        <AddTodo submitHandler={submitHandler}/>
+        <View style = {styles.list}>
+          <FlatList 
+          data = {todos}
+          renderItem = {({  item  }) => (
+          <TodoItem item ={item}  pressHandler= {pressHandler}/>
+          )}
+          />
+        </View>
+      </View>
+    </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:  '#EFEFF9',
+ //   alignItems: 'center',
+  //  justifyContent: "center",
+  },
+  content: {
+    padding : 40,
+  },
+  list: {
+    marginTop: 20,
+  }
+});
+
